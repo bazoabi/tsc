702766159352e1e70268e8f859eea1126cfc8e60
Bader Zoabi <bazoabi@microsoft.com>
2023-05-13 20:03:47 +0300

    applying automated compilation and execution modules/process

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,5 +1,14 @@
 "use strict";
 console.log('hey there!');
 const logSomething = () => {
-    console.log('something');
+    console.log('somethingFIRST');
 };
+function logSomething2() {
+    console.log('something2');
+}
+const hiddenFunc = function logSomething3() {
+    console.log('something3');
+};
+logSomething();
+logSomething2();
+hiddenFunc();
