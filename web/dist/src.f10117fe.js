4209cac92a5638fb2339ec5a4612b93efadb7544
Bader Zoabi <bazoabi@microsoft.com>
2023-05-19 21:25:23 +0300

    adding trigger for events handling

--- web/dist/src.f10117fe.js
+++ web/dist/src.f10117fe.js
@@ -140,6 +140,15 @@ var User = /** @class */function () {
     handlers.push(callback);
     this.events[eventName] = handlers;
   };
+  User.prototype.trigger = function (eventName) {
+    var handlers = this.events[eventName];
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+    handlers.forEach(function (callback) {
+      callback();
+    });
+  };
   return User;
 }();
 exports.User = User;
@@ -154,10 +163,16 @@ var user = new User_1.User({
   name: 'myname',
   age: 20
 });
-user.on('change', function () {});
-user.on('change', function () {});
-user.on('laskjdf', function () {});
-console.log(user);
+user.on('change', function () {
+  console.log('Change #1');
+});
+user.on('change', function () {
+  console.log('Change #2');
+});
+user.on('save', function () {
+  console.log('Save was triggered');
+});
+user.trigger('change');
 },{"./models/User":"src/models/User.ts"}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -183,7 +198,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55075" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57194" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
